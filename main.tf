b4dd4c01981502916590964144878867bd3adb90
pp0blooming <pp0blooming@gmail.com>
2022-07-05 17:06:52 +0000

    tmp checkout tst

--- main.tf
+++ main.tf
@@ -16,7 +16,8 @@ provider "aws" {
 
 resource "aws_s3_bucket" "b" {
   # bucket = "my-tf-test-bucket-sl-varun88"
-  bucket = "kkaty3-us-east-1"
+  # bucket = "kkaty3-us-east-1"
+  bucket = 'kkaty3-us-east-1-20220705'
 
   tags = {
     Name        = "My bucket"
